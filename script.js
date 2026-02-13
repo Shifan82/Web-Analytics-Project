ac14145c1dc327e14111f326b15de43481969d42
Shifan82 <shifanbadgujar82@gmail.com>
2026-02-13 23:25:11 +0530

    Restore feedback functionality and default records

--- script.js
+++ script.js
@@ -52,44 +52,4 @@ if (table) {
     });
 }
 
-		### CLICKY ANTI-ADBLOCK PROXY - https://clicky.com/help/proxy
 		
-		# IMPORTANT NOTE: Incoming X-Forwarded-For headers are supported by default.
-		# To disable this, replace all "$proxy_add_x_forwarded_for" references with "$remote_addr".
-		
-		# DNS RESOLVER - if already defined elsewhere, this can optionally be deleted.
-		resolver 1.1.1.1;
-		
-		# COOKIES - override cookie header to just send the service-related first party cookies.
-		set $cookie "";
-		if ($cookie__cky_ignore) {
-			set $cookie "_cky_ignore=$cookie__cky_ignore; _cky_osa=$cookie__cky_osa";
-		}
-		
-		# JAVASCRIPT TRACKING CODE
-		location = /49d928229b0ddb.js {
-			proxy_pass https://static.getclicky.com/js?in=%2Fc93bca17921309;
-			proxy_connect_timeout 10s;
-			proxy_http_version 1.1;
-			proxy_ssl_server_name on;
-			proxy_set_header Host static.getclicky.com;
-			proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-			proxy_set_header Cookie "";
-		}
-		
-		# JAVASCRIPT BEACON
-		location = /c93bca17921309 {
-			proxy_pass https://in.getclicky.com/in.php;
-			proxy_connect_timeout 10s;
-			proxy_http_version 1.1;
-			proxy_ssl_server_name on;
-			proxy_set_header Host in.getclicky.com;
-			proxy_set_header X-Forwarded-Proto $scheme;
-			proxy_set_header X-Forwarded-Host  $host;
-			proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;
-			proxy_set_header Cookie $cookie;
-		}
-		
-		### / CLICKY 
-      }
-}
